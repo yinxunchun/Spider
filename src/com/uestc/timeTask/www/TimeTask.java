7320696b06260bf70462979950e2b4122d72df06
xunchun <xunchun@yinxunchun>
2015-01-13 15:02:35 +0800

    定时器

---
+++ src/com/uestc/timeTask/www/TimeTask.java
@@ -0,0 +1,13 @@
+package com.uestc.timeTask.www;
+
+import java.util.TimerTask;
+
+public class TimeTask extends TimerTask{
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
